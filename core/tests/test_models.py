24ae5e80c88dc32500fb948564967fbe5dba6a98
David Gomez <davidgomez@Davids-MacBook-Pro.local>
2022-02-17 13:55:17 -0500

    Initial commit with custom admin model with Tests

---
+++ core/tests/test_models.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def sample_user(email="user@bodegonasusalud.com", password="testpass"):
+    """Creates a simple user"""
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "user@bodegonasusalud.com"
+        password = "testpass"
+        user = sample_user()
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "user@BODEGONASUSALUD.com"
+        user = get_user_model().objects.create_user(email, "test123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
+
+    def test_create_new_super_user(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            email='user@bodegonasusalud.com',
+            password='password123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
